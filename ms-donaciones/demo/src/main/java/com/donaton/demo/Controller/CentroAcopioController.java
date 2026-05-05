112d1ba78ba39dd6f7fd34fcf2c826c3138af4d4
yesi <yes.jara@duocuc.cl>
2026-05-05 17:59:35 -0400

    listo swagger

--- ms-donaciones/demo/src/main/java/com/donaton/demo/Controller/CentroAcopioController.java
+++ ms-donaciones/demo/src/main/java/com/donaton/demo/Controller/CentroAcopioController.java
@@ -3,6 +3,8 @@ package com.donaton.demo.Controller;
 import com.donaton.demo.DTO.AcopioRequestDTO;
 import com.donaton.demo.DTO.AcopioResponseDTO;
 import com.donaton.demo.Service.CentroAcopioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/centros")
+@Tag(name = "Centro de acopio", description = "Gestion de centros de acopio")
 public class CentroAcopioController {
 
     private final CentroAcopioService centroAcopioService;
@@ -20,27 +23,32 @@ public class CentroAcopioController {
         this.centroAcopioService =centroAcopioService;
     }
 
+    @Operation(summary = "crear un centro de acopio")
     @PostMapping
     public ResponseEntity<AcopioResponseDTO> crear(@Valid @RequestBody AcopioRequestDTO request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(centroAcopioService.crear(request));
     }
 
+    @Operation(summary = "Listar todo los  centro de acopio")
     @GetMapping
     public ResponseEntity<List<AcopioResponseDTO>> listar() {
         return ResponseEntity.ok(centroAcopioService.listar());
     }
 
+    @Operation(summary = "Obterner el centro de acopio por id")
     @GetMapping("/{id}")
     private ResponseEntity<AcopioResponseDTO>obtenerPorId(@PathVariable Long id){
         return ResponseEntity.ok(centroAcopioService.obtenerPorId(id));
     }
 
+    @Operation(summary = "Listar centros de acopio por región")
     @GetMapping("/region/{region}")
     public ResponseEntity<List<AcopioResponseDTO>>listarPorRegion(@PathVariable String region){
         return ResponseEntity.ok(centroAcopioService.listarPorRegion(region));
 
     }
 
+    @Operation(summary = "Listar centro de acopio activos/disponibles")
     @GetMapping("/activos")
     public ResponseEntity<List<AcopioResponseDTO>>listarActivos(){
         return ResponseEntity.ok(centroAcopioService.listarActivo());
